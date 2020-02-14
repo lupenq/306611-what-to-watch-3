99cb4c7f6042c0131bd608c787aeeb0c4d3223a7
Ilya Lupen <skinilya@gmail.com>
2020-02-14 10:16:59 +0300

    Переименовал Main и App

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,21 +1,17 @@
-import Main from '../main/main';
+import Main from '../main/main.jsx';
 
-const movieCardTitleHandler = (e) => {
-  e.preventDefault();
-};
 
 const App = ({filmsList, promoSettings}) => {
   return (
     <Main
       promoSettings={promoSettings}
       filmsList={filmsList}
-      onMovieCardTitleClick={movieCardTitleHandler}
     />
   );
 };
 
 App.propTypes = {
-  promoSettings: PropTypes.shape({
+  promoSettings: PropTypes.objectOf({
     name: PropTypes.string.isRequired,
     genre: PropTypes.string.isRequired,
     date: PropTypes.string.isRequired
